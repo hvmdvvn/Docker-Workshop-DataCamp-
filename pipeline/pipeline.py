848a4f2bfb703940a7df3eb4dc4745f314e96259
Hamdan S. <hamdansethi.ai@gmail.com>
2026-01-15 05:08:30 +0000

    Dockerized Ingested Pipeline

--- pipeline/pipeline.py
+++
@@ -1,9 +0,0 @@
-import sys
-import pandas as pd
-
-month = sys.argv[1]
-
-df = pd.DataFrame({'day': [1, 2, 3], 'num_passengers': [10, 20, 30]})
-df['month'] = month
-
-df.to_parquet(f'passengers_{month}.parquet', index=False)
